0b7da093a66d29ba66364daeac7543a579d10322
Bilo <mateojimenezfamaart@gmail.com>
2025-02-13 10:15:14 -0500

    Puzzles

--- BubblePuzzle/Assets/MainMenu.cs
+++ BubblePuzzle/Assets/MainMenu.cs
@@ -13,4 +13,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
 }
